b6caf5157f4c956acd8856178eb29aee94269d09
Wong Jiang Fung <kakarukeys@gmail.com>
2021-01-30 22:53:31 +0800

    rearranged tests in describe block, added missing await

--- test/controllers/TeacherController.e2e.test.js
+++ test/controllers/TeacherController.e2e.test.js
@@ -20,293 +20,295 @@ afterAll(() => {
   afterAction();
 });
 
-describe('teacher | register', () => {
-  beforeAll(async () => {
-    await clearData();
+describe('teacher controller', () => {
+  describe('register', () => {
+    beforeAll(async () => {
+      await clearData();
 
-    /* test fixture */
-    await Teacher.bulkCreate([
-      { email: 'benleong@hotmail.com' },
-    ]);
+      /* test fixture */
+      await Teacher.bulkCreate([
+        { email: 'benleong@hotmail.com' },
+      ]);
 
-    await Student.bulkCreate([
-      { email: 'bob@gmail.com' },
-      { email: 'jane@gmail.com' },
-    ]);
+      await Student.bulkCreate([
+        { email: 'bob@gmail.com' },
+        { email: 'jane@gmail.com' },
+      ]);
+    });
+
+    test('invalid teacher', async () => {
+      const res = await request(api)
+        .post('/api/register')
+        .send({
+          teacher: 'invalid@hotmail.com',
+          students: [
+            'bob@gmail.com',
+            'jane@gmail.com',
+          ],
+        })
+        .set('Content-Type', 'application/json');
+
+      expect(res.body).toStrictEqual({ message: 'teacher email is not registered' });
+      expect(res.status).toBe(400);
+
+      const regs = await TeacherStudent.findAll();
+      expect(regs.length).toBe(0);
+    });
+
+    test('invalid student', async () => {
+      const res = await request(api)
+        .post('/api/register')
+        .send({
+          teacher: 'benleong@hotmail.com',
+          students: [
+            'bob@gmail.com',
+            'invalid@gmail.com',
+          ],
+        })
+        .set('Content-Type', 'application/json');
+
+      expect(res.body.message).toContain('some student emails are not registered');
+      expect(res.status).toBe(400);
+
+      const regs = await TeacherStudent.findAll();
+      expect(regs.length).toBe(0);
+    });
+
+    test('all entities exist', async () => {
+      let res = await request(api)
+        .post('/api/register')
+        .send({
+          teacher: 'benleong@hotmail.com',
+          students: [
+            'bob@gmail.com',
+            'jane@gmail.com',
+          ],
+        })
+        .set('Content-Type', 'application/json');
+
+      expect(res.status).toBe(204);
+
+      let regs = await TeacherStudent.count();
+      expect(regs).toBe(2);
+
+      // register again
+      res = await request(api)
+        .post('/api/register')
+        .send({
+          teacher: 'benleong@hotmail.com',
+          students: [
+            'bob@gmail.com',
+            'jane@gmail.com',
+          ],
+        })
+        .set('Content-Type', 'application/json');
+
+      expect(res.status).toBe(204);
+
+      regs = await TeacherStudent.count();
+      expect(regs).toBe(2);
+    });
   });
 
-  test('invalid teacher', async () => {
-    const res = await request(api)
-      .post('/api/register')
-      .send({
-        teacher: 'invalid@hotmail.com',
+  describe('commonStudents', () => {
+    beforeAll(async () => {
+      await clearData();
+
+      /* test fixture */
+      const teachers = await Promise.all(_.map([
+        { email: 'benleong@hotmail.com' },
+        { email: 'ken@hotmail.com' },
+        { email: 'ryu@hotmail.com' },
+      ], (d) => Teacher.create(d)));
+
+      const students = await Promise.all(_.map([
+        { email: 'bob@gmail.com' },
+        { email: 'jane@gmail.com' },
+        { email: 'eric@gmail.com' },
+        { email: 'nancy@gmail.com' },
+        { email: 'tracy@gmail.com' },
+      ], (d) => Student.create(d)));
+
+      await TeacherStudent.bulkCreate([
+        { TeacherId: teachers[0].id, StudentId: students[0].id },
+        { TeacherId: teachers[0].id, StudentId: students[1].id },
+        { TeacherId: teachers[0].id, StudentId: students[2].id },
+        { TeacherId: teachers[1].id, StudentId: students[1].id },
+        { TeacherId: teachers[1].id, StudentId: students[2].id },
+        { TeacherId: teachers[1].id, StudentId: students[3].id },
+        { TeacherId: teachers[2].id, StudentId: students[4].id },
+      ]);
+    });
+
+    test('0 teacher', async () => {
+      const res = await request(api)
+        .get('/api/commonstudents?');
+
+      expect(res.status).toBe(400);
+    });
+
+    test('non-existing teacher', async () => {
+      const res = await request(api)
+        .get('/api/commonstudents?teacher=invalid@hotmail.com');
+
+      expect(res.body.message).toBe('at least one registered teacher email is required');
+      expect(res.status).toBe(400);
+    });
+
+    test('1 teacher', async () => {
+      const res = await request(api)
+        .get('/api/commonstudents?teacher=benleong@hotmail.com');
+
+      expect(res.body).toStrictEqual({
         students: [
           'bob@gmail.com',
+          'eric@gmail.com',
           'jane@gmail.com',
         ],
-      })
-      .set('Content-Type', 'application/json');
+      });
+      expect(res.status).toBe(200);
+    });
 
-    expect(res.body).toStrictEqual({ message: 'teacher email is not registered' });
-    expect(res.status).toBe(400);
+    test('1 teacher + non-existing teacher', async () => {
+      const res = await request(api)
+        .get('/api/commonstudents?teacher=benleong%40hotmail.com&teacher=invalid%40hotmail.com');
 
-    const regs = await TeacherStudent.findAll();
-    expect(regs.length).toBe(0);
-  });
-
-  test('invalid student', async () => {
-    const res = await request(api)
-      .post('/api/register')
-      .send({
-        teacher: 'benleong@hotmail.com',
+      expect(res.body).toStrictEqual({
         students: [
           'bob@gmail.com',
-          'invalid@gmail.com',
-        ],
-      })
-      .set('Content-Type', 'application/json');
-
-    expect(res.body.message).toContain('some student emails are not registered');
-    expect(res.status).toBe(400);
-
-    const regs = await TeacherStudent.findAll();
-    expect(regs.length).toBe(0);
-  });
-
-  test('all entities exist', async () => {
-    let res = await request(api)
-      .post('/api/register')
-      .send({
-        teacher: 'benleong@hotmail.com',
-        students: [
-          'bob@gmail.com',
+          'eric@gmail.com',
           'jane@gmail.com',
         ],
-      })
-      .set('Content-Type', 'application/json');
+      });
+      expect(res.status).toBe(200);
+    });
 
-    expect(res.status).toBe(204);
+    test('2 teachers', async () => {
+      const res = await request(api)
+        .get('/api/commonstudents?teacher=benleong%40hotmail.com&teacher=ken%40hotmail.com');
 
-    let regs = await TeacherStudent.count();
-    expect(regs).toBe(2);
-
-    // register again
-    res = await request(api)
-      .post('/api/register')
-      .send({
-        teacher: 'benleong@hotmail.com',
+      expect(res.body).toStrictEqual({
         students: [
-          'bob@gmail.com',
+          'eric@gmail.com',
           'jane@gmail.com',
         ],
-      })
-      .set('Content-Type', 'application/json');
-
-    expect(res.status).toBe(204);
-
-    regs = await TeacherStudent.count();
-    expect(regs).toBe(2);
-  });
-});
-
-describe('teacher | commonStudents', () => {
-  beforeAll(async () => {
-    await clearData();
-
-    /* test fixture */
-    const teachers = await Promise.all(_.map([
-      { email: 'benleong@hotmail.com' },
-      { email: 'ken@hotmail.com' },
-      { email: 'ryu@hotmail.com' },
-    ], (d) => Teacher.create(d)));
-
-    const students = await Promise.all(_.map([
-      { email: 'bob@gmail.com' },
-      { email: 'jane@gmail.com' },
-      { email: 'eric@gmail.com' },
-      { email: 'nancy@gmail.com' },
-      { email: 'tracy@gmail.com' },
-    ], (d) => Student.create(d)));
-
-    await TeacherStudent.bulkCreate([
-      { TeacherId: teachers[0].id, StudentId: students[0].id },
-      { TeacherId: teachers[0].id, StudentId: students[1].id },
-      { TeacherId: teachers[0].id, StudentId: students[2].id },
-      { TeacherId: teachers[1].id, StudentId: students[1].id },
-      { TeacherId: teachers[1].id, StudentId: students[2].id },
-      { TeacherId: teachers[1].id, StudentId: students[3].id },
-      { TeacherId: teachers[2].id, StudentId: students[4].id },
-    ]);
-  });
-
-  test('0 teacher', async () => {
-    const res = await request(api)
-      .get('/api/commonstudents?');
-
-    expect(res.status).toBe(400);
-  });
-
-  test('non-existing teacher', async () => {
-    const res = await request(api)
-      .get('/api/commonstudents?teacher=invalid@hotmail.com');
-
-    expect(res.body.message).toBe('at least one registered teacher email is required');
-    expect(res.status).toBe(400);
-  });
-
-  test('1 teacher', async () => {
-    const res = await request(api)
-      .get('/api/commonstudents?teacher=benleong@hotmail.com');
-
-    expect(res.body).toStrictEqual({
-      students: [
-        'bob@gmail.com',
-        'eric@gmail.com',
-        'jane@gmail.com',
-      ],
+      });
+      expect(res.status).toBe(200);
     });
-    expect(res.status).toBe(200);
-  });
 
-  test('1 teacher + non-existing teacher', async () => {
-    const res = await request(api)
-      .get('/api/commonstudents?teacher=benleong%40hotmail.com&teacher=invalid%40hotmail.com');
+    test('no overlap', async () => {
+      const res = await request(api)
+        .get('/api/commonstudents?teacher=benleong%40hotmail.com&teacher=ken%40hotmail.com&teacher=ryu%40hotmail.com');
 
-    expect(res.body).toStrictEqual({
-      students: [
-        'bob@gmail.com',
-        'eric@gmail.com',
-        'jane@gmail.com',
-      ],
+      expect(res.body).toStrictEqual({
+        students: [],
+      });
+      expect(res.status).toBe(200);
     });
-    expect(res.status).toBe(200);
   });
 
-  test('2 teachers', async () => {
-    const res = await request(api)
-      .get('/api/commonstudents?teacher=benleong%40hotmail.com&teacher=ken%40hotmail.com');
+  describe('suspend', () => {
+    beforeAll(async () => {
+      await clearData();
 
-    expect(res.body).toStrictEqual({
-      students: [
-        'eric@gmail.com',
-        'jane@gmail.com',
-      ],
+      /* test fixture */
+      await Student.bulkCreate([
+        { email: 'bob@gmail.com' },
+        { email: 'jane@gmail.com' },
+      ]);
     });
-    expect(res.status).toBe(200);
-  });
 
-  test('no overlap', async () => {
-    const res = await request(api)
-      .get('/api/commonstudents?teacher=benleong%40hotmail.com&teacher=ken%40hotmail.com&teacher=ryu%40hotmail.com');
+    test('invalid student', async () => {
+      const res = await request(api)
+        .post('/api/suspend')
+        .send({ student: 'invalid@gmail.com' })
+        .set('Content-Type', 'application/json');
 
-    expect(res.body).toStrictEqual({
-      students: [],
+      expect(res.body.message).toContain('student email is not registered');
+      expect(res.status).toBe(400);
+
+      const totalSuspended = await Student.count({ where: { suspended: true } });
+      expect(totalSuspended).toBe(0);
     });
-    expect(res.status).toBe(200);
-  });
-});
 
-describe('teacher | suspend', () => {
-  beforeAll(async () => {
-    await clearData();
+    test('suspend bob', async () => {
+      const res = await request(api)
+        .post('/api/suspend')
+        .send({ student: 'bob@gmail.com' })
+        .set('Content-Type', 'application/json');
 
-    /* test fixture */
-    Student.bulkCreate([
-      { email: 'bob@gmail.com' },
-      { email: 'jane@gmail.com' },
-    ]);
-  });
+      expect(res.status).toBe(204);
 
-  test('invalid student', async () => {
-    const res = await request(api)
-      .post('/api/suspend')
-      .send({ student: 'invalid@gmail.com' })
-      .set('Content-Type', 'application/json');
+      const { count: totalSuspended, rows: students } = await Student
+        .findAndCountAll({ where: { suspended: true } });
 
-    expect(res.body.message).toContain('student email is not registered');
-    expect(res.status).toBe(400);
-
-    const totalSuspended = await Student.count({ where: { suspended: true } });
-    expect(totalSuspended).toBe(0);
-  });
-
-  test('suspend bob', async () => {
-    const res = await request(api)
-      .post('/api/suspend')
-      .send({ student: 'bob@gmail.com' })
-      .set('Content-Type', 'application/json');
-
-    expect(res.status).toBe(204);
-
-    const { count: totalSuspended, rows: students } = await Student
-      .findAndCountAll({ where: { suspended: true } });
-
-    expect(totalSuspended).toBe(1);
-    expect(students[0].email).toBe('bob@gmail.com');
-  });
-});
-
-describe('teacher | retrieveForNotifications', () => {
-  beforeAll(async () => {
-    await clearData();
-
-    /* test fixture */
-    const teachers = await Promise.all(_.map([
-      { email: 'benleong@hotmail.com' },
-      { email: 'ken@hotmail.com' },
-      { email: 'ryu@hotmail.com' },
-    ], (d) => Teacher.create(d)));
-
-    const students = await Promise.all(_.map([
-      { email: 'bob@gmail.com' },
-      { email: 'jane@gmail.com', suspended: true },
-      { email: 'eric@gmail.com' },
-      { email: 'nancy@gmail.com', suspended: true },
-      { email: 'tracy@gmail.com' },
-      { email: 'miko@gmail.com' },
-    ], (d) => Student.create(d)));
-
-    await TeacherStudent.bulkCreate([
-      { TeacherId: teachers[0].id, StudentId: students[0].id },
-      { TeacherId: teachers[0].id, StudentId: students[1].id },
-      { TeacherId: teachers[0].id, StudentId: students[2].id },
-      { TeacherId: teachers[1].id, StudentId: students[1].id },
-      { TeacherId: teachers[1].id, StudentId: students[2].id },
-      { TeacherId: teachers[1].id, StudentId: students[3].id },
-      { TeacherId: teachers[2].id, StudentId: students[4].id },
-    ]);
-  });
-
-  test('invalid teacher', async () => {
-    const res = await request(api)
-      .post('/api/retrievefornotifications')
-      .send({
-        teacher: 'invalid@hotmail.com',
-        notification: '... hello ... @bob@gmail.com @jane@gmail.com @nancy@gmail.com @tracy@gmail.com @moko@gmail.com @miko@gmail.com',
-      })
-      .set('Content-Type', 'application/json');
-
-    expect(res.body).toStrictEqual({ message: 'teacher email is not registered' });
-    expect(res.status).toBe(400);
-  });
-
-  test('retrieve for notifications', async () => {
-    const res = await request(api)
-      .post('/api/retrievefornotifications')
-      .send({
-        teacher: 'benleong@hotmail.com',
-        notification: '... hello ... @bob@gmail.com @nancy@gmail.com @tracy@gmail.com @moko@gmail.com @miko@gmail.com',
-      })
-      .set('Content-Type', 'application/json');
-
-    expect(res.body).toStrictEqual({
-      recipients: [
-        'bob@gmail.com',
-        'eric@gmail.com',
-        'miko@gmail.com',
-        'tracy@gmail.com',
-      ],
+      expect(totalSuspended).toBe(1);
+      expect(students[0].email).toBe('bob@gmail.com');
     });
-    expect(res.status).toBe(200);
+  });
+
+  describe('retrieveForNotifications', () => {
+    beforeAll(async () => {
+      await clearData();
+
+      /* test fixture */
+      const teachers = await Promise.all(_.map([
+        { email: 'benleong@hotmail.com' },
+        { email: 'ken@hotmail.com' },
+        { email: 'ryu@hotmail.com' },
+      ], (d) => Teacher.create(d)));
+
+      const students = await Promise.all(_.map([
+        { email: 'bob@gmail.com' },
+        { email: 'jane@gmail.com', suspended: true },
+        { email: 'eric@gmail.com' },
+        { email: 'nancy@gmail.com', suspended: true },
+        { email: 'tracy@gmail.com' },
+        { email: 'miko@gmail.com' },
+      ], (d) => Student.create(d)));
+
+      await TeacherStudent.bulkCreate([
+        { TeacherId: teachers[0].id, StudentId: students[0].id },
+        { TeacherId: teachers[0].id, StudentId: students[1].id },
+        { TeacherId: teachers[0].id, StudentId: students[2].id },
+        { TeacherId: teachers[1].id, StudentId: students[1].id },
+        { TeacherId: teachers[1].id, StudentId: students[2].id },
+        { TeacherId: teachers[1].id, StudentId: students[3].id },
+        { TeacherId: teachers[2].id, StudentId: students[4].id },
+      ]);
+    });
+
+    test('invalid teacher', async () => {
+      const res = await request(api)
+        .post('/api/retrievefornotifications')
+        .send({
+          teacher: 'invalid@hotmail.com',
+          notification: '... hello ... @bob@gmail.com @jane@gmail.com @nancy@gmail.com @tracy@gmail.com @moko@gmail.com @miko@gmail.com',
+        })
+        .set('Content-Type', 'application/json');
+
+      expect(res.body).toStrictEqual({ message: 'teacher email is not registered' });
+      expect(res.status).toBe(400);
+    });
+
+    test('retrieve for notifications', async () => {
+      const res = await request(api)
+        .post('/api/retrievefornotifications')
+        .send({
+          teacher: 'benleong@hotmail.com',
+          notification: '... hello ... @bob@gmail.com @nancy@gmail.com @tracy@gmail.com @moko@gmail.com @miko@gmail.com',
+        })
+        .set('Content-Type', 'application/json');
+
+      expect(res.body).toStrictEqual({
+        recipients: [
+          'bob@gmail.com',
+          'eric@gmail.com',
+          'miko@gmail.com',
+          'tracy@gmail.com',
+        ],
+      });
+      expect(res.status).toBe(200);
+    });
   });
 });
